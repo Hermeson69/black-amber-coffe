62f08752a1003942f7f9f62f8a530b0cbaf8d3d1
Hermeson Alves <hermesonalves256@gmail.com>
2026-06-07 20:16:15 -0300

    feat: add order router

--- src/routes/v1.route.ts
+++ src/routes/v1.route.ts
@@ -3,14 +3,16 @@ import { healthRoutes } from "@/modules/health/health.routes";
 import { authRoutes } from "@/modules/auth/auth.routes";
 import { userRoutes } from "@/modules/user/user.routes";
 import { workerRoutes } from "@/modules/worker/worker.routes";
+import { orderRoutes } from "@/modules/order/order.routes";
 import setupSwagger from "@/shared/swagger";
 
 const routes = Router();
 
-routes.use("/api",healthRoutes);
+routes.use("/api", healthRoutes);
 routes.use("/api", authRoutes);
 routes.use("/api", userRoutes);
 routes.use("/api", workerRoutes);
+routes.use("/api", orderRoutes);
 
 setupSwagger(routes);
 
